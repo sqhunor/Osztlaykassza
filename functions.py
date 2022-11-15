b66edbeb6e65b9d6237717269ae720dcaa1e1c0d
Skultéty Hunor 10.B <skultetyh@gmail.com>
2022-11-15 09:21:33 +0100

    nevsor

--- functions.py
+++ functions.py
@@ -1,4 +1,6 @@
 from os import system
+from data import * 
+
 def menu():
     system('cls')  
     print('0 - Kilép')
@@ -7,3 +9,10 @@ def menu():
     print('3 - Legutóbbi befizetés/kiadás')
     print('4 - Számlán lévő egyenleg')
     return input('Választás:')
+
+def  showNameList():
+    system('cls')
+    print('Versenyzők')
+    for nev in nevsor:
+        print(f'\t{nev}')
+    input()
---
+++ main.py
@@ -0,0 +1,13 @@
+from functions import *
+
+choice = ''
+while choice != '0':
+    choice = menu()
+    if choice == '1':
+        pass
+    elif choice == '2':
+        showNameList()
+    elif choice == '3':
+        pass
+    elif choice == '4':
+        pass
